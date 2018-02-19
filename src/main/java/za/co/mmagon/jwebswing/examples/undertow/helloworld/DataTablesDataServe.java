e0bc360591f1d2e2531f2b7be7ae8be6d5fc2a67
Marc Magon <ged_marc@hotmail.com>
2018-02-19 20:41:39 +0200

    first commit

---
+++ src/main/java/za/co/mmagon/jwebswing/examples/undertow/helloworld/DataTablesDataServe.java
@@ -0,0 +1,52 @@
+package za.co.mmagon.jwebswing.examples.undertow.helloworld;
+
+import za.co.mmagon.jwebswing.plugins.jqdatatable.DataTableData;
+import za.co.mmagon.jwebswing.plugins.jqdatatable.events.DataTableDataFetchEvent;
+import za.co.mmagon.jwebswing.plugins.jqdatatable.search.DataTableSearchRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataTablesDataServe extends DataTableDataFetchEvent
+{
+	@Override
+	public MyData returnData(DataTableSearchRequest searchRequest)
+	{
+		MyData data = new MyData();
+		data.loadData(searchRequest.getStart(),searchRequest.getLength());
+		return data;
+	}
+
+	class MyData extends DataTableData
+	{
+		public List<List<String>> data;
+
+		public MyData()
+		{
+			setRecordsTotal(100);
+			setRecordsFiltered(50);
+		}
+
+		public void loadData(Integer start, Integer length)
+		{
+			data = new ArrayList<>();
+			if(start == null)
+				start = 0;
+			if (length == null) {
+				length = 10;
+			}
+
+			for (int i = 0,j = start; i < length; i++,j++)
+			{
+				List<String> cells = new ArrayList<>();
+				cells.add("Data " + (j + 1));
+				cells.add("Data " + (j + 1));
+				cells.add("Data " + (j + 1));
+				cells.add("Data " + (j + 1));
+				cells.add("Data " + (j + 1));
+				data.add(cells);
+			}
+		}
+	}
+
+}
